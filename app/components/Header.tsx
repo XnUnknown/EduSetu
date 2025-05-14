2e3414a421ef4b5ba4f7c54781da44386c930c88
XnUnknown <nishantvishwakarma6789@gmail.com>
2025-05-14 17:44:43 +0530

    Mobile Header Overflow Fixed

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,14 +4,17 @@ import Link from 'next/link';
 import "../variable.css"
 export default function Header() {
   return (
-    <header className="bg-white shadow-sm py-4 px-6 flex justify-between items-center navbar" style={{marginBottom:30}}>
-      <Link href="/" className="text-2xl font-bold text-blue-700" >EduSetu</Link>
-      <nav className="space-x-4">
-        <Link href="/about" className="text-black">About</Link>
-        <Link href="/volunteer" className="text-black">Volunteer</Link>
-        <Link href="/school" className="text-black">School</Link>
-        {/* <Link href="/admin" className="text-black">Admin</Link> */}
-      </nav>
-    </header>
+<header className="bg-white shadow-sm py-4 px-6 flex flex-col sm:flex-row justify-between items-center navbar gap-4 sm:gap-0" style={{ marginBottom: 30 }}>
+  <Link href="/" className="text-2xl font-bold text-blue-700 text-center sm:text-left break-words">
+    EduSetu
+  </Link>
+  
+  <nav className="flex flex-wrap justify-center sm:justify-end gap-4">
+    <Link href="/about" className="text-black whitespace-nowrap">About</Link>
+    <Link href="/volunteer" className="text-black whitespace-nowrap">Volunteer</Link>
+    <Link href="/school" className="text-black whitespace-nowrap">School</Link>
+    {/* <Link href="/admin" className="text-black">Admin</Link> */}
+  </nav>
+</header>
   );
 }
